f6f440e3c079cfe7f0a383cddc9be0c3923dea10
andreyirk <sokolik_85@list.ru>
2020-04-23 16:47:47 +0300

    check style

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -2,9 +2,9 @@ package ru.job4j.condition;
 
 public class SqArea {
     public static double square(int p, int k) {
-        double h = p/(2*(k + 1));
-        double L = k * h;
-        return h * L;
+        double h = p / (2 * (k + 1));
+        double l = k * h;
+        return h * l;
     }
 
     public static void main(String[] args) {
