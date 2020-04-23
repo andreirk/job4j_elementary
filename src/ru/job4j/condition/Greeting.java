415dc120487539899fcb749504077684cd612b1b
andreyirk <sokolik_85@list.ru>
2020-04-23 09:37:07 +0300

    Strings

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+
+        System.out.println(idea + "but I'am a newbie");
+
+        int year = 2020;
+
+        idea = idea + year;
+
+        System.out.println(idea);
+    }
+}
