0dfb5d0a2f22eaeb23236a4c02b9e62586e470eb
andreyirk <sokolik_85@list.ru>
2020-04-25 14:13:51 +0300

    defragment array

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index; // указатель, на не null ячейку.
+                for (int innerI = point; innerI < array.length; innerI++) {
+                    if (array[innerI] != null) {
+                        SwitchArray.swap(array, index, innerI);
+                        break;
+                    }
+                }
+                // переместить первую не null ячейку
+                // Здесь нужен цикл while
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
