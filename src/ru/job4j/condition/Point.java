69bf2b2a9d3c6e2ef3323afb634d6ba63441a869
andreyirk <sokolik_85@list.ru>
2020-04-26 14:28:13 +0300

    point 3d

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,21 +1,45 @@
 package ru.job4j.condition;
 
-public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double hipo1 = x2 - x1;
-        double hipo2 = y2 - y1;
-        double squareOfSum = Math.pow(hipo1, 2) + Math.pow(hipo2, 2);
-        double rsl = Math.sqrt(squareOfSum);
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
-        return rsl;
+public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    private int z;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point point2) {
+        return sqrt(pow(point2.x - this.x, 2) + pow(point2.y - this.y, 2));
+    }
+
+    public double distance3d(Point point2, Point point3) {
+        return sqrt(pow(point2.x - this.x, 2) + pow(point2.y - this.y, 2) + (point3.z - this.z) );
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(5, 6, 2, 3);
-        double result3 = Point.distance(2, 11, 3, 8);
-        System.out.println("result (0, 0) to (2, 0) " + result);
-        System.out.println("result ,(2 11), (3, 8) " + result3);
-        System.out.println("result (5, 6), (2, 3) " + result2);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -8,9 +8,28 @@ public class PointTest {
 
     @Test
     public void distance() {
-        int out = (int) Point.distance(0, 0, 10, 0);
-        int expected = 10;
+        Point point1 = new Point(5, 3);
+        Point point2 = new Point(2, 334);
 
-        assertEquals(out, expected);
+        double out = (double) point1.distance(point2);
+        double expected = 10;
+
+        assertEquals(out, expected, 0.1);
+
+
+    }
+    @Test
+    public void distance3d() {
+        Point point1 = new Point(5, 3, 45);
+        Point point2 = new Point(2, 334, 12);
+        Point point3 = new Point(22, 34, 2);
+
+
+        double out = (double) point1.distance3d(point2, point3);
+        double expected = 330.94;
+
+        assertEquals(out, expected, 0.1);
+
+
     }
 }
